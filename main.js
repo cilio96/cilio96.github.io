ee66d04837528bc0d73fd551f7d9a3a0b59041c4
cilio96 <41536587+cilio96@users.noreply.github.com>
2020-04-17 16:36:16 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -12,13 +12,12 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
   // Output the result in an element with id="demo"
-  document.getElementById("counter").innerHTML = days + "d:" + hours + "h:"
+  document.getElementById("counter").innerHTML = days + "g:" + hours + "h:"
   + minutes + "m:" + seconds + "s";
     
   // If the count down is over, write some text 
@@ -30,10 +29,11 @@ var x = setInterval(function() {
   }
 }, 1000);
 
+
 var width = document.body.clientWidth; 
 //display random images 
 if(width > 1024){
-var imageArray = ['img/contedesk.jpg','img/dimaio.jpg', 'img/salvinidesk.jpg', 'img/mattarelladesk.jpg', 'img/melonidesk.jpg', 'img/zingarettidesk.jpg'],
+var imageArray = ['img/contedesk.jpg','img/dimaio.jpg', 'img/mattarelladesk.jpg', 'img/melonidesk.jpg', 'img/zingarettidesk.jpg', 'img/salvinidesk.jpg'],
     selectImg = imageArray[Math.floor(Math.random() * imageArray.length)];
     document.body.style.backgroundImage = 'url(' + selectImg + ')';
 }
@@ -42,3 +42,37 @@ else {
     selectImg = imageArray[Math.floor(Math.random() * imageArray.length)]; 
      document.body.style.backgroundImage = 'url(' + selectImg + ')';
 }
+
+/*//gestione video 
+if(width > 1024){
+var iframe = document.getElementById("myFrame");
+
+iframe.style.position = 'absolute';
+iframe.style.top='20%'; 
+iframe.style.left='5%'; 
+    if(selectImg.localeCompare('img/salvinidesk.jpg') == 0){
+    iframe.style.position = 'fixed';
+    iframe.style.width = "360px"; 
+    iframe.style.height = "260px"; 
+    iframe.style.top='18%'; 
+    iframe.style.left='35%'; 
+    }
+    else if(selectImg.localeCompare('img/contedesk.jpg') == 0 || selectImg.localeCompare('img/dimaio.jpg') == 0){
+    iframe.style.position = 'fixed';
+    iframe.style.width = "360px"; 
+    iframe.style.height = "260px"; 
+    iframe.style.top='28%'; 
+    iframe.style.left='5%'; 
+    }
+    else if(selectImg.localeCompare('img/mattarelladesk.jpg') == 0 || selectImg.localeCompare('img/melonidesk.jpg') == 0 || selectImg.localeCompare('img/zingarettidesk.jpg') == 0){
+    iframe.style.position = 'fixed';
+    iframe.style.width = "360px"; 
+    iframe.style.height = "260px"; 
+    iframe.style.top='30%'; 
+    iframe.style.left='5%'; 
+    }
+}
+else{
+    var iframe = document.getElementById("myFrame");
+    iframe.style.visibility = "hidden"; 
+}*/
